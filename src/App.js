a0e7824590364fada04efd47abc35822c62f4046
MaxvG115 <maxivillegas115@gmail.com>
2023-01-05 22:24:50 -0600

    Added heroicons and Projects component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
     <main className="text-white bg-teal-900 body-font">
       <About />
+      <Projects />
     </main>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+    {
+      title: "IOS app for Gudalajara's Food Bank",
+      subtitle: "Swift | Firebase | Stripe",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./testgif.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "CRUD app for ITESM MCY program",
+      subtitle: "Ruby on Rails",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./testgif.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "Micropost sample app",
+      subtitle: "Ruby on Rails",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./testgif.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Job Search app for IJALTI",
+      subtitle: ".NET | MySQL | Bootstrap",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./testgif.gif",
+      link: "https://pythonbootcamp.com",
+    },
+
+  ];
+
+  export const skills = [
+    "HTML, CSS, TailwindCSS",
+    "React",
+    "Node",
+    "Ruby/Rails",
+    "C++",
+    "Swift",
+  ];
